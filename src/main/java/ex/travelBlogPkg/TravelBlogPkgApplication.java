7d9ba53d9ad56ec0ca3cefdb445a241e8cb6ea71
Johan Kövamees <johan.kovamees@gmail.com>
2023-10-27 13:05:19 +0200

    Initial spring commit

---
+++ src/main/java/ex/travelBlogPkg/TravelBlogPkgApplication.java
@@ -0,0 +1,13 @@
+package ex.travelBlogPkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBlogPkgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelBlogPkgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ex/travelBlogPkg/TravelBlogPkgApplicationTests.java
@@ -0,0 +1,13 @@
+package ex.travelBlogPkg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBlogPkgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
